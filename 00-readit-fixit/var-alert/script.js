89735ff445c5f359177a98e2d6616641fcfbf604
Giovanni Overmeire <101796485+Gioke@users.noreply.github.com>
2022-06-15 11:24:19 +0200

    Made the var alert

---
+++ 00-readit-fixit/var-alert/script.js
@@ -0,0 +1,18 @@
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    let title = "hello, World"
+    alert(title)
+
+
+
+
+
+
+
+})();;
+
+
+
