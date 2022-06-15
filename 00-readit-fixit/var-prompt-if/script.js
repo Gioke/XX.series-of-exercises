c4d47178fee5629ddbddab1aa067cc1e3ae7818a
Giovanni Overmeire <101796485+Gioke@users.noreply.github.com>
2022-06-15 12:14:27 +0200

    Made the var-prompt-if

---
+++ 00-readit-fixit/var-prompt-if/script.js
@@ -0,0 +1,15 @@
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    const cake = prompt('Do you want cake?');
+    if (cake === 'yes') {
+        alert('Awesome, here is some cake for u to enjoy!!');
+    } else {
+        alert('More cake for me then :p !');
+    }
+
+
+
+})();
