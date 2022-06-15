f0fc7706a78715fa031daae0f77be0a934ee1ec5
Giovanni Overmeire <101796485+Gioke@users.noreply.github.com>
2022-06-15 11:39:12 +0200

    Made the var-prompt

---
+++ 00-readit-fixit/var-prompt/script.js
@@ -0,0 +1,9 @@
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    const name = prompt('What is ur name?');
+    alert("Hello " + name + "!");
+
+})();
