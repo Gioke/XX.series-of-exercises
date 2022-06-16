8ce184889606d95a96c016539c58780a8bd6b402
Giovanni Overmeire <101796485+Gioke@users.noreply.github.com>
2022-06-16 12:10:52 +0200

    Made excersize one from colors

--- 02-colors/01-change-bcg-one/script.js
+++ 02-colors/01-change-bcg-one/script.js
@@ -3,6 +3,28 @@
 
 (function() {
 
-    // your code here
+    const btn1 = document.querySelector('#red');
+
+    btn1.addEventListener('click', function onClick(event) {
+
+        document.body.style.backgroundColor = 'red';});
+
+    const btn2 = document.querySelector('#green');
+
+    btn2.addEventListener('click', function onClick(event) {
+
+        document.body.style.backgroundColor = 'green';});
+
+    const btn3 = document.querySelector('#yellow');
+
+    btn3.addEventListener('click', function onClick(event) {
+
+        document.body.style.backgroundColor = 'yellow';});
+
+    const btn4 = document.querySelector('#blue');
+
+    btn4.addEventListener('click', function onClick(event) {
+
+        document.body.style.backgroundColor = 'blue';});
 
 })();
